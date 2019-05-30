3fdebaa66a15c0b9b57435a3c3d9a3bea9ef8b90
Diego Reboucas <DREBOUCAS@voegol.com.br>
2019-05-30 17:43:57 -0300

    Impementando o dominio

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,35 @@
+﻿using QuickBuy.Dominio.Enumeradores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+
+        }
+        public bool ECartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaCredito; }
+
+        }
+        public bool EDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+
+        }
+        public bool ENaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+
+        }
+
+    }
+}
